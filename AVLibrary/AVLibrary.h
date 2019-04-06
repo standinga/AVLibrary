7a3410cace9dd10f18ac1186beb79ab7f8933668
michal bojanowicz <boramaapps@gmail.com>
2019-04-06 23:59:36 +0200

    Initial Commit

---
+++ AVLibrary/AVLibrary.h
@@ -0,0 +1,19 @@
+//
+//  AVLibrary.h
+//  AVLibrary
+//
+//  Created by michal on 06/04/2019.
+//  Copyright © 2019 michal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AVLibrary.
+FOUNDATION_EXPORT double AVLibraryVersionNumber;
+
+//! Project version string for AVLibrary.
+FOUNDATION_EXPORT const unsigned char AVLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AVLibrary/PublicHeader.h>
+
+
