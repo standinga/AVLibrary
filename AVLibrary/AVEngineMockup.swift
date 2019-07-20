f84236c6eb22128a5044b6786cf70095c1a7c242
michal bojanowicz <boramaapps@gmail.com>
2019-07-20 03:26:35 +0200

    refactored

--- AVLibrary/AVEngineMockup.swift
+++ AVLibrary/AVEngineMockup.swift
@@ -22,66 +22,32 @@ class AVEngineMockup: NSObject, AVEngineProtocol {
     var isRunning = true
     var currentCameraPosition = AVCaptureDevice.Position.back
     var format: MockupAVFormat
+    
     private var timer: Timer?
-    private var timevalue: Int64 = 46735832821083
     private var sampleBuffer: CMSampleBuffer!
     
     var isFocusLocked = false
-    var blackImage: UIImage!
-    var previous = CFAbsoluteTimeGetCurrent()
+    var previousTimestamp = CFAbsoluteTimeGetCurrent()
     var startTime = CFAbsoluteTimeGetCurrent() - 0.02
     
     init(lockingQueue: DispatchQueue) {
         self.lockingQueue = lockingQueue
         
         self.format = MockupAVFormat()
-        let frame = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 600, height: 800))
-        let ciimage = CIImage(color: CIColor(color: .black))
-        let cgImage = CIContext().createCGImage(ciimage, from: frame)!
-        blackImage = UIImage(cgImage: cgImage)
-        let image = UIImage(named: "sky", in: Bundle(for: type(of: self)), compatibleWith: nil)!
+        previousTimestamp = CFAbsoluteTimeGetCurrent()
+        
         super.init()
-        previous = CFAbsoluteTimeGetCurrent()
+        
+        let image = UIImage(named: "sky", in: Bundle(for: type(of: self)), compatibleWith: nil)!
+        
         let pixelBuffer = createCVPixelBuffer(from: image)
-        
         setAttachments(to: pixelBuffer)
-        
         let sampleBuffer = createSampleBufferWith(pixelBuffer: pixelBuffer)
         
-        print(sampleBuffer)
         self.sampleBuffer = sampleBuffer
         format.mockFormatDescription = CMSampleBufferGetFormatDescription(sampleBuffer)!
         timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(onTime), userInfo: nil, repeats: true)
         timer?.fire()
-    }
-    
-    func createCVPixelBuffer(from image: UIImage) -> CVPixelBuffer {
-        guard let cgImage = image.cgImage else {
-            fatalError("cgImage nil \(#function)")
-        }
-        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue as CFBoolean,
-                     kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue as CFBoolean,
-                     kCVPixelBufferIOSurfacePropertiesKey: [:] as CFDictionary // without kCVPixelBufferIOSurfacePropertiesKey key the AVAssetWriter won't append sample buffers with error: AVAssetWriterInput append fails with error code -11800 AVErrorUnknown -12780
-                     ] as CFDictionary
-        var pbuff: CVPixelBuffer? = nil
-        let status = CVPixelBufferCreate(kCFAllocatorDefault, cgImage.width, cgImage.height, kCVPixelFormatType_32ARGB, attrs, &pbuff)
-        
-        guard status == kCVReturnSuccess else { fatalError("status error \(status)")}
-        guard let pixelBuffer = pbuff else { fatalError("pixelbuffer")}
-        
-        CVPixelBufferLockBaseAddress(pixelBuffer, [])
-        guard let pixelAddress = CVPixelBufferGetBaseAddress(pixelBuffer) else {
-             fatalError("pointer null \(#function)")
-        }
-        let colorSpace = CGColorSpaceCreateDeviceRGB()
-        guard let context = CGContext(data: pixelAddress, width: cgImage.width, height: cgImage.height, bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer), space: colorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) else {
-            fatalError("context: \(#function)")
-        }
-        UIGraphicsPushContext(context)
-        image.draw(in: CGRect(x: 0, y: 0, width: cgImage.width, height: cgImage.height))
-        UIGraphicsPopContext()
-        CVPixelBufferUnlockBaseAddress(pixelBuffer, [])
-        return pixelBuffer
     }
     
     override init() {
@@ -104,40 +70,41 @@ class AVEngineMockup: NSObject, AVEngineProtocol {
     }
     
     @objc func onTime() {
-        guard let pixelBuffer = blackImage.pixelBuffer else {
-            fatalError("can't create pixelbuffer")
-        }
-        timevalue += 50000000
-        var formatDescription: CMFormatDescription?
-        CMVideoFormatDescriptionCreateForImageBuffer(allocator: nil, imageBuffer: pixelBuffer, formatDescriptionOut: &formatDescription)
-        guard let fd = formatDescription else {
-            fatalError("can't create formatDescription")
-        }
         
-        var count: CMItemCount = 0
-        var osStatus = CMSampleBufferGetSampleTimingInfoArray(sampleBuffer, entryCount: 0, arrayToFill: nil, entriesNeededOut: &count)
-        
-        var info = [CMSampleTimingInfo](repeating: CMSampleTimingInfo(duration: .zero, presentationTimeStamp: .zero, decodeTimeStamp: .zero), count: count)
-        
-        osStatus = CMSampleBufferGetSampleTimingInfoArray(sampleBuffer, entryCount: count, arrayToFill: &info, entriesNeededOut: &count)
         let now = CFAbsoluteTimeGetCurrent()
-        let delta = now - previous
+        let delta = now - previousTimestamp
         startTime += delta
-        previous = now
+        previousTimestamp = now
         let timestamp = CMTime(seconds: startTime, preferredTimescale: 1000000000)
-        for i in 0..<count {
-            info[i].presentationTimeStamp = timestamp
-            info[i].decodeTimeStamp = .invalid
-        }
-        var out: CMSampleBuffer?
-        CMSampleBufferCreateCopyWithNewTiming(allocator: kCFAllocatorDefault, sampleBuffer: sampleBuffer, sampleTimingEntryCount: count, sampleTimingArray: &info, sampleBufferOut: &out)
-        guard let retimedSampleBuffer = out else {
-            fatalError("no retimedSampleBuffer")
-        }
-//        delegate?.onPixelBuffer(pixelBuffer, timestamp: time, formatDescription: fd)
+        let retimedSampleBuffer = retimeSampleBuffer(sampleBuffer, timestamp: timestamp)
+        
+        //        delegate?.onPixelBuffer(pixelBuffer, timestamp: time, formatDescription: fd)
         delegate?.onSampleBuffer(retimedSampleBuffer, connection: AVCaptureConnection(inputPorts: [], output: MockupAVCaptureOutput()), timestamp: timestamp, output: MockupAVCaptureOutput(), isVideo: true)
     }
     
+    func toggleCamera() {
+        
+    }
+    
+    func orientationChanged(rawValue: Int) {
+        
+    }
+    
+    func toggleFocus() {
+        
+    }
+    
+    func changeCameraFormat(_ format: AVCaptureDevice.Format?, fps: Int) {
+        
+    }
+    
+    func setupAVCapture(_ index: AVCaptureDevice.Position, fps: Int, savedFormatString: String?, videoOrientation: AVCaptureVideoOrientation) {
+        let session = AVCaptureSession()
+        delegate?.didStartRunning(format: format, session: session, avData: AVEngineData(format: format, session: session, cameraPosition: .front, fps: 30, focus: .autoFocus, lensPosition: 1.4, videoOrientation: .portrait))
+    }
+}
+
+private extension AVEngineMockup {
     func setAttachments(to pixelBuffer: CVPixelBuffer) {
         let dict = [kCVImageBufferColorPrimariesKey: kCVImageBufferColorPrimaries_ITU_R_709_2, kCVImageBufferTransferFunctionKey: kCVImageBufferTransferFunction_ITU_R_709_2, kCVImageBufferYCbCrMatrixKey: kCVImageBufferYCbCrMatrix_ITU_R_601_4] as CFDictionary
         
@@ -168,24 +135,54 @@ class AVEngineMockup: NSObject, AVEngineProtocol {
         return sampleBuffer
     }
     
-    func toggleCamera() {
+    func retimeSampleBuffer(_ sampleBuffer: CMSampleBuffer, timestamp: CMTime) -> CMSampleBuffer {
+        var count: CMItemCount = 0
+        var osStatus = CMSampleBufferGetSampleTimingInfoArray(sampleBuffer, entryCount: 0, arrayToFill: nil, entriesNeededOut: &count)
+        guard osStatus == 0 else {
+            fatalError("CMSampleBufferGetSampleTimingInfoArray \(osStatus)")
+        }
         
+        var info = [CMSampleTimingInfo](repeating: CMSampleTimingInfo(duration: .zero, presentationTimeStamp: .zero, decodeTimeStamp: .zero), count: count)
+        
+        osStatus = CMSampleBufferGetSampleTimingInfoArray(sampleBuffer, entryCount: count, arrayToFill: &info, entriesNeededOut: &count)
+        for i in 0..<count {
+            info[i].presentationTimeStamp = timestamp
+            info[i].decodeTimeStamp = .invalid
+        }
+        var out: CMSampleBuffer?
+        CMSampleBufferCreateCopyWithNewTiming(allocator: kCFAllocatorDefault, sampleBuffer: sampleBuffer, sampleTimingEntryCount: count, sampleTimingArray: &info, sampleBufferOut: &out)
+        guard let retimedSampleBuffer = out else {
+            fatalError("no retimedSampleBuffer")
+        }
+        return retimedSampleBuffer
     }
     
-    func orientationChanged(rawValue: Int) {
+    func createCVPixelBuffer(from image: UIImage) -> CVPixelBuffer {
+        guard let cgImage = image.cgImage else {
+            fatalError("cgImage nil \(#function)")
+        }
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue as CFBoolean,
+                     kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue as CFBoolean,
+                     kCVPixelBufferIOSurfacePropertiesKey: [:] as CFDictionary // without kCVPixelBufferIOSurfacePropertiesKey key the AVAssetWriter won't append sample buffers with error: AVAssetWriterInput append fails with error code -11800 AVErrorUnknown -12780
+            ] as CFDictionary
+        var pbuff: CVPixelBuffer? = nil
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, cgImage.width, cgImage.height, kCVPixelFormatType_32ARGB, attrs, &pbuff)
         
-    }
-    
-    func toggleFocus() {
+        guard status == kCVReturnSuccess else { fatalError("status error \(status)")}
+        guard let pixelBuffer = pbuff else { fatalError("pixelbuffer")}
         
-    }
-    
-    func changeCameraFormat(_ format: AVCaptureDevice.Format?, fps: Int) {
-        
-    }
-    
-    func setupAVCapture(_ index: AVCaptureDevice.Position, fps: Int, savedFormatString: String?, videoOrientation: AVCaptureVideoOrientation) {
-        let session = AVCaptureSession()
-        delegate?.didStartRunning(format: format, session: session, avData: AVEngineData(format: format, session: session, cameraPosition: .front, fps: 30, focus: .autoFocus, lensPosition: 1.4, videoOrientation: .portrait))
+        CVPixelBufferLockBaseAddress(pixelBuffer, [])
+        guard let pixelAddress = CVPixelBufferGetBaseAddress(pixelBuffer) else {
+            fatalError("pointer null \(#function)")
+        }
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        guard let context = CGContext(data: pixelAddress, width: cgImage.width, height: cgImage.height, bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer), space: colorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) else {
+            fatalError("context: \(#function)")
+        }
+        UIGraphicsPushContext(context)
+        image.draw(in: CGRect(x: 0, y: 0, width: cgImage.width, height: cgImage.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer, [])
+        return pixelBuffer
     }
 }
